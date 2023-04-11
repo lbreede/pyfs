e447a81f3587ff335a9fd1c5a8e8cfe78620b414
lbreede <lennart.breede@gmail.com>
2023-04-10 23:48:33 -0700

    Initial commit

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import logging
 from typing import Optional
 
-from nodes import Directory, File
+from nodes import Directory, File  # type: ignore
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
